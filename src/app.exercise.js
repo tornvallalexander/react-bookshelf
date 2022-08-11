4e86d35938e0a96f16b3df969f1f3f0ecae5254a
Alexander Törnvall <alexander.tornvall@icloud.com>
2022-08-11 14:51:17 +0200

    feat(states): loading and error states with useAsync hook

--- src/app.exercise.js
+++ src/app.exercise.js
@@ -6,6 +6,8 @@ import * as auth from 'auth-provider'
 import {AuthenticatedApp} from './authenticated-app'
 import {UnauthenticatedApp} from './unauthenticated-app'
 import {client} from './utils/api-client'
+import {useAsync} from './utils/hooks';
+import {FullPageSpinner} from './components/lib';
 
 async function getUser() {
   let user = null
@@ -20,17 +22,32 @@ async function getUser() {
 }
 
 function App() {
-  const [user, setUser] = React.useState(null)
+  const {
+    data: user,
+    setData: setUser,
+    run,
+    error,
+    isError,
+    isLoading,
+    isIdle,
+  } = useAsync()
 
   React.useEffect(() => {
-    (async () => {
-      setUser(await getUser())
-    })()
-  }, [])
+    run(getUser())
+  }, [run])
 
   const login = form => auth.login(form).then(u => setUser(u))
   const register = form => auth.register(form).then(u => setUser(u))
   const logout = () => auth.logout().finally(() => setUser(null))
+
+  if (isLoading || isIdle) return <FullPageSpinner />
+
+  if (isError) return (
+    <div style={{ color: "orangered"}}>
+      <p>Uh oh... there's a problem.</p>
+      <pre>{error}</pre>
+    </div>
+  )
 
   return user
     ? <AuthenticatedApp user={user} logout={logout} />
