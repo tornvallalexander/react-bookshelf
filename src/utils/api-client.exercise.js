b67fa3355a701b68753159c317c8b6e5f9b1aa03
Alexander Törnvall <alexander.tornvall@icloud.com>
2022-08-11 15:07:43 +0200

    feat(api): dealing with unauthorized requests - logging out

--- src/utils/api-client.exercise.js
+++ src/utils/api-client.exercise.js
@@ -1,16 +1,36 @@
+import * as auth from '../auth-provider';
+
 const apiURL = process.env.REACT_APP_API_URL
 
-function client(endpoint, {token, headers: customHeaders, ...customConfig} = {}) {
+function client(
+  endpoint,
+  {data, token, headers: customHeaders, ...customConfig} = {},
+  retry = false,
+) {
   const config = {
-    method: 'GET',
+    method: data ? 'POST' : 'GET',
     headers: {
       Authorization: token ? `Bearer ${token}` : undefined,
+      'Content-Type': data ? 'application/json' : undefined,
       ...customHeaders,
     },
     ...customConfig,
   }
 
   return window.fetch(`${apiURL}/${endpoint}`, config).then(async response => {
+    if (response.status === 401) {
+      if (retry) {
+        await auth.logout()
+        window.location.assign(window.location)
+        return
+      } else {
+        return client(
+          endpoint,
+          {data, token, headers: customHeaders, ...customConfig},
+          true
+        )
+      }
+    }
     const data = await response.json()
     if (response.ok) {
       return data
