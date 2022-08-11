f4baa4019260d93cdc89f7a5692eb665108be4e1
Alexander Törnvall <alexander.tornvall@icloud.com>
2022-08-11 13:16:08 +0200

    feat(auth): improved, new function to get user when we reload instead of setting it to null

--- src/app.exercise.js
+++ src/app.exercise.js
@@ -5,9 +5,28 @@ import * as React from 'react'
 import * as auth from 'auth-provider'
 import {AuthenticatedApp} from './authenticated-app'
 import {UnauthenticatedApp} from './unauthenticated-app'
+import {client} from './utils/api-client'
+
+async function getUser() {
+  let user = null
+
+  const token = await auth.getToken()
+  if (token) {
+    const data = await client('me', {token})
+    user = data?.user
+  }
+
+  return user
+}
 
 function App() {
   const [user, setUser] = React.useState(null)
+
+  React.useEffect(() => {
+    (async () => {
+      setUser(await getUser())
+    })()
+  }, [])
 
   const login = form => auth.login(form).then(u => setUser(u))
   const register = form => auth.register(form).then(u => setUser(u))
