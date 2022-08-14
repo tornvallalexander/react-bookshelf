0700e55551137c4b0b76c375c638bdc8b3f90145
Alexander Törnvall <alexander.tornvall@icloud.com>
2022-08-14 13:43:34 +0200

    chore(config): react query config wrapper

--- src/index.exercise.js
+++ src/index.exercise.js
@@ -3,12 +3,28 @@ import './bootstrap'
 import * as React from 'react'
 import {createRoot} from 'react-dom/client'
 import {App} from './app'
+import {ReactQueryConfigProvider} from 'react-query';
+
+const queryConfig = {
+  queries: {
+    refetchOnWindowFocus: false,
+    useErrorBoundary: true,
+    retry: (failureCount, error) => {
+      if (error.status === 404) return false
+      else return failureCount < 2;
+    }
+  }
+}
 
 // ignore the rootRef in this file. I'm just doing it here to make
 // the tests I write to check your work easier.
 export const rootRef = {}
 loadDevTools(() => {
   const root = createRoot(document.getElementById('root'))
-  root.render(<App />)
+  root.render(
+    <ReactQueryConfigProvider config={queryConfig}>
+      <App />
+    </ReactQueryConfigProvider>
+  )
   rootRef.current = root
 })
