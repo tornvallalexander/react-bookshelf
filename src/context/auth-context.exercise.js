f7857a0d4f8ffe2262fbfe7ba734368e9fd28cc2
Alexander Törnvall <alexander.tornvall@icloud.com>
2022-08-15 11:00:09 +0200

    refactor(auth): moving auth logic from app to own context file

--- src/context/auth-context.exercise.js
+++ src/context/auth-context.exercise.js
@@ -1,14 +1,75 @@
-// 🐨 create and export a React context variable for the AuthContext
-// 💰 using React.createContext
-import * as React from "react"
+/** @jsx jsx */
+import {jsx} from '@emotion/core'
+
+import * as React from 'react'
+import * as auth from '../auth-provider';
+import {client} from '../utils/api-client';
+import {useAsync} from '../utils/hooks';
+import {FullPageErrorFallback, FullPageSpinner} from '../components/lib';
 
 const AuthContext = React.createContext()
+AuthContext.displayName = "AuthContext"
 
 function useAuth() {
-  return React.useContext(AuthContext)
+  const context = React.useContext(AuthContext)
+  if (context === undefined) {
+    throw new Error('useAuth must be used within an AuthProvider')
+  }
+  return context
+}
+
+async function getUser() {
+  let user = null
+
+  const token = await auth.getToken()
+  if (token) {
+    const data = await client('me', {token})
+    user = data.user
+  }
+
+  return user
+}
+
+function AuthProvider(props) {
+  const {
+    data: user,
+    error,
+    isLoading,
+    isIdle,
+    isError,
+    isSuccess,
+    run,
+    setData,
+  } = useAsync()
+
+  React.useEffect(() => {
+    run(getUser())
+  }, [run])
+
+  const login = form => auth.login(form).then(user => setData(user))
+  const register = form => auth.register(form).then(user => setData(user))
+  const logout = () => {
+    void auth.logout()
+    setData(null)
+  }
+
+  if (isLoading || isIdle) {
+    return <FullPageSpinner />
+  }
+
+  if (isError) {
+    return <FullPageErrorFallback error={error} />
+  }
+
+  if (isSuccess) {
+    const value = {user, login, register, logout}
+    return (
+      <AuthContext.Provider value={value} {...props} />
+    )
+  }
 }
 
 export {
-  AuthContext,
+  AuthProvider,
   useAuth,
 }
--- src/context/index.exercise.js
+++
@@ -1,2 +0,0 @@
-// this module doesn't do anything for the exercise. But you'll use this for
-// the extra credit!
