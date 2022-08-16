65fa0b2fcb5e3d7f37b29874b81ee521a37448c6
Alexander Törnvall <alexander.tornvall@icloud.com>
2022-08-16 09:42:30 +0200

    perf(user): run getUser function outside auth provider for better perf

--- src/context/auth-context.exercise.js
+++ src/context/auth-context.exercise.js
@@ -22,6 +22,7 @@ async function getUser() {
 const AuthContext = React.createContext()
 AuthContext.displayName = 'AuthContext'
 
+const userPromise = getUser()
 function AuthProvider(props) {
   const {
     data: user,
@@ -36,14 +37,6 @@ function AuthProvider(props) {
   } = useAsync()
 
   React.useEffect(() => {
-    // we need to call getUser() sooner.
-    // 🐨 move the next line to just outside the AuthProvider
-    // 🦉 this means that as soon as this module is imported,
-    // it will start requesting the user's data so we don't
-    // have to wait until the app mounts before we kick off
-    // the request.
-    // We're moving from "Fetch on render" to "Render WHILE you fetch"!
-    const userPromise = getUser()
     run(userPromise)
   }, [run])
 
