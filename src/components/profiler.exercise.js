49c5bd1c5981aea33199c250f3e0d88102e8567d
Alexander Törnvall <alexander.tornvall@icloud.com>
2022-08-16 08:50:45 +0200

    perf(profiler): custom profiler to send data to backend

--- src/components/profiler.exercise.js
+++ src/components/profiler.exercise.js
@@ -1,1 +1,43 @@
-// this is for extra credit
+import * as React from 'react'
+import {client} from '../utils/api-client';
+
+let queue = []
+
+setInterval(sendProfileQueue, 5000)
+
+function sendProfileQueue() {
+  if (!queue.length) {
+    return Promise.resolve({success: true})
+  }
+  const queueToSend = [...queue]
+  queue = []
+  return client('profile', {data: {queueToSend}})
+}
+
+function Profiler({phases, ...props}) {
+  function reportProfile(
+    id,
+    phase,
+    actualDuration,
+    baseDuration,
+    startTime,
+    commitTime,
+    interactions,
+  ) {
+    console.log(phase)
+    if (!phases || phases.includes(phase)) {
+      queue.push({
+        id,
+        phase,
+        actualDuration,
+        baseDuration,
+        startTime,
+        commitTime,
+        interactions,
+      })
+    }
+  }
+  return <React.Profiler onRender={reportProfile} {...props} />
+}
+
+export {Profiler}
